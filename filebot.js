e43aa08f2d740f8522183c3c23690855f0a60ed4
aypn <80340450+D3NUVO@users.noreply.github.com>
2023-05-10 13:52:09 +0530

    need changes with got and axios

--- filebot.js
+++ filebot.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
+const got = require('got');
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
-const input = require("input"); // npm i input
+const input = require("input");
+const stream = require('stream');
+const { promisify } = require('util');
 const sessionFile = 'session.session';
 const path = require('path');
 
-
 require('dotenv').config();
-
 
 const apiId = process.env.API_ID;
 const apiHash = process.env.API_HASH;
-
+const pipeline = promisify(stream.pipeline);
 
 (async () => {
     let stringSession = "";
@@ -41,27 +42,40 @@ const apiHash = process.env.API_HASH;
         console.log("Session string saved to", sessionFile);
     }
 
+    const downloadFile = async (url, outputPath, onProgress) => {
+        const downloadStream = got.stream(url);
+        downloadStream.on('downloadProgress', onProgress);
+        await pipeline(
+            downloadStream,
+            fs.createWriteStream(outputPath)
+        );
+    };
 
-
-
-
-
-
-
-
-
-
-
-    const uploadfile = async () => {
+    const uploadFile = async () => {
         try {
-            const filePath = path.resolve('https://nw7.seedr.cc/ff_get/1505192518/A.Perfect.Pairing.2022.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4?st=amItx6lvgVwsq-dZZ_DPYQ&e=1683789391');
-            const message = await client.sendMessage('me', { message: 'Uploading file: 0%' });
+            const fileUrl = 'https://nw7.seedr.cc/ff_get/1505192518/A.Perfect.Pairing.2022.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4?st=amItx6lvgVwsq-dZZ_DPYQ&e=1683789391';
+            const localFilePath = 'downloaded_file.mp4';
+
+            const message = await client.sendMessage('me', { message: 'Downloading file: 0%' });
+
+            // Download the file
+            await downloadFile(fileUrl, localFilePath, async (progress) => {
+                console.log(`Download progress: ${Math.round(progress.percent * 100)}%`);
+                const text = `Downloading file: ${Math.round(progress.percent * 100)}%`;
+
+                // Update the message with the download progress
+                await client.editMessage('me', message, text);
+            });
+
+            console.log('File downloaded:', localFilePath);
+
+            await client.editMessage('me', message, 'Uploading file: 0%');
             await client.sendFile('me', {
-                file: filePath,
+                file: localFilePath,
                 forceDocument: true,
                 progressCallback: async (progress) => {
                     console.log(`Upload progress: ${Math.round(progress * 100)}%`);
-                    const text = `Uploading file: ${progressPercentage}%`;
+                    const text = `Uploading file: ${Math.round(progress * 100)}%`;
 
                     // Edit the message with the new progress
                     await client.editMessage('me', message, text);
@@ -70,10 +84,8 @@ const apiHash = process.env.API_HASH;
         } catch (error) {
             console.log(error.message);
         }
-    }
-
+    };
 
     await client.sendMessage("me", { message: "Hello!" });
-    uploadfile();
-    //   await new Promise(solve=>setTimeout(solve, 10000)); // Wait 10 seconds before sending the next message
+    uploadFile();
 })();
