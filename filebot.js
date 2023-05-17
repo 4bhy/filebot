8ed11b1b05f15eef798be899140f9e5f8dd2ac61
aypn <80340450+D3NUVO@users.noreply.github.com>
2023-05-17 11:56:15 +0530

    added all things

--- filebot.js
+++ filebot.js
@@ -1,13 +1,14 @@
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
+const { NewMessage } = require("telegram/events")
 const input = require('input')
-
-
+const sentmsg = require('./functions')
 require('dotenv').config();
 
 const apiId = Number(process.env.API_ID);
 const apiHash = process.env.API_HASH;
 const stringSession = (process.env.SESSION);
+const chatid = (process.env.CHAT_ID)
 
 
 const client = new TelegramClient(new StringSession(stringSession), apiId, apiHash, {
@@ -25,9 +26,21 @@ const client = new TelegramClient(new StringSession(stringSession), apiId, apiHa
     });
     console.log("You should now be connected.");
 
+    async function eventPrint(event) {
+        try {
+            const message = event.message.message;
+            console.log("Here is the message", message)
+            if (message == "/eclipse_java@Brototype_bot") {
+                await client.sendMessage(chatid, { message: "Here is the link for Eclipse: https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2023-03/R/eclipse-inst-jre-win64.exe" });
+            } else if (message == "/mingw@Brototype_bot") {
+                await client.sendMessage(chatid, { message: "Here is the link for cygwin: https://cygwin.com/setup-x86_64.exe" });
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
+    client.addEventHandler(eventPrint, new NewMessage({ chats: [chatid] }));
 
-
-
-    sentmsg(client);
+    // sentmsg(client);
 })();
